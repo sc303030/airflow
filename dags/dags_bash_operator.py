44d9f53cae4ae43fc83cee13506df8a5799abd0b
이경현 <lkh6035@naver.com>
2024-10-14 20:18:41 +0900

    [241014] bash operator 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    # [START howto_operator_bash]
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+    # [END howto_operator_bash]
+
+    bash_t1 >> bash_t2
