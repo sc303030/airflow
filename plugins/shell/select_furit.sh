4220d7d8ecad70d7f40092d4336c557b68a7d344
이경현 <lkh6035@naver.com>
2024-10-17 15:54:13 +0900

    [241017] add plugins fold

---
+++ plugins/shell/select_furit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1
+if [$FRUIT == APPLE];then
+    echo "apple!"
+elif [$FRUIT == ORANGE];then
+    echo "orange!"
+elif [$FRUIT == GRAPH];then
+    echo "graph"
+else
+    echo "other fruit"
+fi
